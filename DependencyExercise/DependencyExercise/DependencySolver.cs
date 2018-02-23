2baacd34be84456d55d169157cc1d1e23c3f4d8a
Ali <aymomeni@gmail.com>
2018-02-23 01:12:39 -0700

    bug fixes and simplification needs to happen and will work on more tests tomorrow morning

--- DependencyExercise/DependencyExercise/DependencySolver.cs
+++ DependencyExercise/DependencyExercise/DependencySolver.cs
@@ -13,12 +13,16 @@ using System.Text.RegularExpressions;
  */
 namespace DependencyExercise
 {
-    class DependencySolver
+    public class DependencySolver
     {
         // Member variables
-        // dictionary ? and queue?
+        private int necessaryFollowups = 0;
         private Queue<string> mQueue = new Queue<string>();
         private Dictionary<string, string> mDictionaryDependeeDependent = new Dictionary<string, string>();
+
+        public DependencySolver(){
+
+        }
 
         /// <summary>
         /// returns a comma seperated string of package names in the order of install,
@@ -27,10 +31,48 @@ namespace DependencyExercise
         /// <param name="inputArrOfStringTuples"></param>
         /// <returns>string of packages in topological order, or null if an invalid string array was given 
         /// (dependency is cyclic or has more than one dependent per dependee)</returns>
-        public static string solvePackageDependencies(string[] inputArrOfStringTuples)
+        public string solvePackageDependencies(string[] inputArrOfStringTuples)
         {
+            if (inputArrOfStringTuples.Length == 0) { return null; }
+            else if (inputArrOfStringTuples.Length == 1) { return inputArrOfStringTuples[0]; }
+        
+            string result = "";
 
-            return null;
+            //TODO: could there only be 2 elements?
+
+            // processing the strings in the array &
+            // populating queue and dict.
+            parseArrHelper(inputArrOfStringTuples);
+
+            string tempKey = "";
+            string tempValue = "";
+            
+            while(mQueue.Count > 0 || necessaryFollowups > 0) {
+                tempKey = mQueue.Dequeue();
+
+                try
+                {
+                    if (mDictionaryDependeeDependent.ContainsKey(tempKey))
+                    {
+                        mDictionaryDependeeDependent.TryGetValue(tempKey, out tempValue);
+                        mQueue.Enqueue(tempValue);
+                        result += tempKey + ", ";
+                        necessaryFollowups--;
+                    }
+                    else if (necessaryFollowups == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        result += tempKey + ", ";
+                    }
+                }
+                catch (ArgumentNullException e) { Console.WriteLine(e.ToString()); }
+                
+            }
+            result += tempKey;
+            return result;
         }
 
         /// <summary>
@@ -38,8 +80,9 @@ namespace DependencyExercise
         /// </summary>
         /// <param name="sArr"></param>
         /// <returns></returns>
-        private static void parseArrHelper(string[] sArr)
+        private void parseArrHelper(string[] sArr)
         {
+            // allowing regex to capture numbers so the testing is easier (I guess one could use only single letters but there is only 26 :)
             Regex regex = new Regex("(?<Dependent>[A-Za-z0-9]+)\\:\\s*(?<Dependee>[A-Za-z0-9]*)");
 
             String tempDependent    = "";
@@ -51,16 +94,26 @@ namespace DependencyExercise
 
                 if (match.Success)
                 {
+                    // grabbing respective groups using regex group functionality
                     tempDependent = match.Groups["Dependent"].Value.Trim();
-                    tempDependee = match.Groups["Dependee"].Value.Trim();
+                    tempDependee  = match.Groups["Dependee"].Value.Trim();
 
-                    // if dependee is empty we know that the node is good starting node as 
-                    // it doesn't have any dependents (a root node) -> add it queue (FIFO)
-
-                    if (tempDependent.Equals(""))
+                    if (tempDependee.Equals(""))
                     {
-                        //mQueue
+                        // if dependee is empty we know that the node is good starting node as 
+                        // it doesn't have any dependents (a root node) -> add it queue (FIFO)
+                        mQueue.Enqueue(tempDependent);
                     }
+                    else
+                    {
+                        // else the node can not be a root node and the dependee of this node
+                        // must be checked for further dependents
+                        mDictionaryDependeeDependent.Add(tempDependee, tempDependent);
+                        necessaryFollowups++;
+                    }
+
+                    // TODO: What if we find at this point that this is cyclic or not based on specification?
+                    // if queue is empty something is wrong or if there is more then one dependent per dependee
 
                     match.NextMatch();
                     Console.WriteLine("Dependent: " + tempDependent + " Dependee: " + tempDependee);
@@ -73,10 +126,12 @@ namespace DependencyExercise
 
         static void Main(string[] args)
         {
-            string[] strArrGiven = new string[] { "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " };
 
-            parseArrHelper(strArrGiven);
-            Console.ReadLine();
+            // Quick check if regex works
+            //string[] strArrGiven = new string[] { "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " };
+
+            //parseArrHelper(strArrGiven);
+            //Console.ReadLine();
         }
     }
 }
--- DependencyExercise/DependencyExerciseTester/UnitTest1.cs
+++ DependencyExercise/DependencyExerciseTester/UnitTest1.cs
@@ -11,7 +11,8 @@ namespace DependencyExerciseTester
         public void TestMethod1()
         {
             string[] strArrGiven = new string[] { "KittenService: CamelCaser", "CamelCaser: " };
-            string actual = "";
+            DependencySolver dS = new DependencySolver();
+            string actual = dS.solvePackageDependencies(strArrGiven);
             string expected = "CamelCaser, KittenService";
 
             Assert.AreEqual(expected, actual, "Test1 Failed");  
@@ -20,9 +21,15 @@ namespace DependencyExerciseTester
         [TestMethod]
         public void TestMethod2()
         {
-            string[] strArrGiven = new string[] { "KittenService: ", "Leetmeme: Cyberportal", "Cyberportal: Ice", "CamelCaser: KittenService", "Fraudstream: Leetmeme", "Ice: " };
-            string actual = "";
-            string expected = "CamelCaser, KittenService";
+            string[] strArrGiven = new string[] { "1: ", 
+                                                  "2: 3", 
+                                                  "3: 4", 
+                                                  "5: 1", 
+                                                  "6: 2", 
+                                                  "4: " };
+            DependencySolver dS = new DependencySolver();
+            string actual = dS.solvePackageDependencies(strArrGiven);
+            string expected = "1, 4, 5, 3, 2, 6";
 
             Assert.AreEqual(expected, actual, "Test2 Failed");
         }
